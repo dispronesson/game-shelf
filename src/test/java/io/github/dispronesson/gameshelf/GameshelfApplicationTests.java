cffbb559e37ff8c3e11096182bc7d73399567f37
dispronesson <dispronesson@mail.ru>
2025-11-27 23:48:56 +0300

    init spring boot

---
+++ src/test/java/io/github/dispronesson/gameshelf/GameshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dispronesson.gameshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
